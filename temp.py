c053919887b599235e2ce503852ed33fa38b8568
965 <wmk153024@gmail.com>
2023-05-23 22:12:50 +0800

    添加练习内容（词云）

--- temp.py
+++ temp.py
@@ -8,11 +8,11 @@ raw_data = open("111.txt", encoding="gbk").read()
 ls = jieba.lcut(raw_data)
 text = ' '.join(ls)
 open("111.txt", encoding="gbk").close()
-mask = np.array(Image.open("mask.png"))
+mask = np.array(Image.open("./resources/mask.png"))
 wc = wordcloud.WordCloud(font_path="msyh.ttc",
                          mask=mask,
                          background_color='white',
                          max_font_size=240,
                          stopwords={'王勃', '一'})
 wc.generate(text)
-wc.to_file("111.png")
+wc.to_file("./results/111.png")
